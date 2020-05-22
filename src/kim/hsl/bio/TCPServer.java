d1815f5cc4e670dad49f4ebff78f1d7488f36d49
han1202012 <octopus_truth@163.com>
2020-05-22 23:15:33 +0800

    update

--- src/kim/hsl/bio/TCPServer.java
+++ src/kim/hsl/bio/TCPServer.java
@@ -1,7 +1,66 @@
 package kim.hsl.bio;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TCPServer {
     public static void main(String[] args) {
+        try {
+            //创建线程池
+            ExecutorService threadPool = Executors.newCachedThreadPool();
+            //创建服务器套接字
+            ServerSocket serverSocket = new ServerSocket(8888);
+            System.out.println("服务器启动,监听 8888 端口");
+            while (true){
+                //阻塞, 等待客户端连接请求 ( 此处是第一个阻塞点 )
+                Socket socket = serverSocket.accept();
+                System.out.println("客户端连接成功");
+                //线程池启动线程
+                threadPool.execute(new ClientRquest(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * 与客户端交互类
+     */
+    static class ClientRquest implements Runnable {
+        private Socket socket;
+        public ClientRquest(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                clientRequest();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                //最终要将 Socket 关闭, 如果出异常继续捕获
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void clientRequest() throws IOException {
+            //获取输入流, 读取客户端写入的信息
+            byte[] buffer = new byte[1024];
+            InputStream is = socket.getInputStream();
+
+            System.out.println("等到客户端请求");
+            //此处会阻塞等待客户端的请求 ( 此处是第二个阻塞点 )
+            int count = is.read(buffer);
+            String request = new String(buffer, 0, count);
+            System.out.println("客户端请求到达 : " + request);
+        }
     }
 }
