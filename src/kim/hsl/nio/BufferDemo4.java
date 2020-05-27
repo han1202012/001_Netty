5635f3a3b512820a32e4e5495873648c9472c144
han1202012 <octopus_truth@163.com>
2020-05-27 12:46:27 +0800

    update

--- src/kim/hsl/nio/BufferDemo4.java
+++ src/kim/hsl/nio/BufferDemo4.java
@@ -1,6 +1,5 @@
 package kim.hsl.nio;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -17,7 +16,8 @@ import java.nio.channels.SocketChannel;
  * 聚合 ( Gathering ) : 通道 ( Channel ) 从 缓冲区数组 中读取数据,
  *                      按照索引从第 0 个缓冲区 ( Buffer ) 开始, 依次读取数据
  *
- * 使用 服务器套接字通道 ( ServerSocketChannel ) 和 套接字通道 ( SocketChannel ) 进行演示
+ * 使用 服务器套接字通道 ( ServerSocketChannel ) 和 套接字通道 ( SocketChannel ) 进行聚合演示
+ * 使用 文件通道 ( FileChannel ) 进行分散演示
  *
  * @author han
  *
@@ -33,16 +33,20 @@ public class BufferDemo4 {
 
             //2 . 创建 2 个 ByteBuffer, 并放入数组中
             ByteBuffer[] buffers = new ByteBuffer[2];
-            buffers[0] = ByteBuffer.allocate(8);
-            buffers[1] = ByteBuffer.allocate(4);
+            buffers[0] = ByteBuffer.allocate(4);
+            buffers[1] = ByteBuffer.allocate(8);
 
             //3 . ServerSocketChannel 阻塞等待客户端连接, 该方法执行后一直阻塞
             SocketChannel socketChannel = serverSocketChannel.accept();
 
             //4 . 阻塞读取数据, 将数据读取到 buffers 缓冲区数组中的缓冲区中
-            long readLen = socketChannel.read(buffers);
+            socketChannel.read(buffers);
 
-            //5 . 将读取到的数据写出到文件中
+            //5 . 将两个 缓冲区 flip 翻转一下, position 设置为 0
+            buffers[0].flip();
+            buffers[1].flip();
+
+            //6 . 将读取到的数据写出到文件中
             FileOutputStream fos = null;
             try {
                 //1 . FileChannel 可以从 FileOutputStream 中获取
@@ -51,8 +55,6 @@ public class BufferDemo4 {
                 //2 . 创建 FileChannel , 从 FileInputStream / FileOutputStream 中可以获取到
                 //FileChannel 是抽象类 , 实际类型是 FileChannelImpl
                 FileChannel fcOut = fos.getChannel();
-
-                //fcOut.write()
 
                 //3 .  将读取到的数据写出到文件中
                 fcOut.write(buffers);
